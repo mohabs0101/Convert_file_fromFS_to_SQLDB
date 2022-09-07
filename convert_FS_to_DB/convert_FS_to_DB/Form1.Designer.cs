ee54f5991633ec038fb5bc53f8a695c92639734f
mohammed abbas <72379773+mohabs0101@users.noreply.github.com>
2022-09-07 09:47:51 +0300

    update

---
+++ convert_FS_to_DB/convert_FS_to_DB/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace convert_FS_to_DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_OFD = new System.Windows.Forms.Button();
+            this.BTN_upload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTN_OFD
+            // 
+            this.BTN_OFD.Location = new System.Drawing.Point(364, 47);
+            this.BTN_OFD.Name = "BTN_OFD";
+            this.BTN_OFD.Size = new System.Drawing.Size(145, 38);
+            this.BTN_OFD.TabIndex = 0;
+            this.BTN_OFD.Text = "choos folder";
+            this.BTN_OFD.UseVisualStyleBackColor = true;
+            this.BTN_OFD.Click += new System.EventHandler(this.BTN_OFD_Click);
+            // 
+            // BTN_upload
+            // 
+            this.BTN_upload.Location = new System.Drawing.Point(364, 131);
+            this.BTN_upload.Name = "BTN_upload";
+            this.BTN_upload.Size = new System.Drawing.Size(145, 23);
+            this.BTN_upload.TabIndex = 1;
+            this.BTN_upload.Text = "upload";
+            this.BTN_upload.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BTN_upload.UseVisualStyleBackColor = true;
+            this.BTN_upload.Click += new System.EventHandler(this.BTN_upload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 450);
+            this.Controls.Add(this.BTN_upload);
+            this.Controls.Add(this.BTN_OFD);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_OFD;
+        private System.Windows.Forms.Button BTN_upload;
+    }
+}
+
---
+++ convert_FS_to_DB/convert_FS_to_DB/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace convert_FS_to_DB
+{
+    public partial class Form1 : Form
+    {
+        public static string photo_con_ = ConfigurationManager.ConnectionStrings["photo_con"].ConnectionString;
+        SqlConnection photo_con = new SqlConnection(photo_con_);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_OFD_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                   string  sourcePath = fbd.SelectedPath;
+                    //array of directories
+                    string[] dirs = Directory.GetDirectories(fbd.SelectedPath);
+
+                    //access first dir 
+                    foreach ( var item in dirs)
+                    {//take its name 
+                        var directoryName = new DirectoryInfo(item).Name;
+                        //read its files
+                        string[] files = Directory.GetFiles(item);
+
+                        //loop on its files and insert them to db
+                        foreach (var item2 in files)
+                        {
+                            string fileName = new FileInfo(item2).Name;
+                            photo_con.Open();
+                            byte[] fileBytes = System.IO.File.ReadAllBytes(item2);
+
+
+                            string photoquery = @"INSERT INTO [dbo].[ArchiveImagesTBL]([bookCode],[filename],[image])
+                                                                      VALUES  (@bookcode,@filename,@img)";
+
+                            SqlCommand photo_cmd = new SqlCommand(photoquery, photo_con);
+
+                            photo_cmd.Parameters.AddWithValue("bookcode", directoryName);
+                            photo_cmd.Parameters.AddWithValue("filename", fileName);
+
+                            photo_cmd.Parameters.AddWithValue("img", fileBytes);
+
+                            photo_cmd.ExecuteNonQuery();
+                            photo_con.Close();
+
+                        }
+                        //go to next dir
+
+
+
+                    }
+                    MessageBox.Show("done");
+                }
+            }
+
+        }
+
+        private void BTN_upload_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
